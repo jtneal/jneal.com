77464f4b40f99da4f0eb879344ecd15ac9b1498f
Jason Neal <jasonneal1@quickenloans.com>
2019-09-10 07:29:58 -0400

    create shared folder

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,8 +3,8 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { FooterMenuComponent } from './footer-menu/footer-menu.component';
 import { HomeComponent } from './home/home.component';
+import { FooterMenuComponent } from './shared/footer-menu/footer-menu.component';
 
 @NgModule({
   bootstrap: [AppComponent],
